d9b6eaad954369fd638e038477a3b1d579a34878
Vladimir Popinov <vladimir@Vladimirs-Air.lan>
2024-01-15 20:22:48 +0000

    Moving Moving

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -20,6 +20,10 @@ class GameScene: SKScene {
         main = self.childNode(withName: "main") as! SKSpriteNode
         
         ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
+                
+        // Add gesture recognizer
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        view.addGestureRecognizer(swipeGesture)
         
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         border.friction = 0
@@ -27,7 +31,41 @@ class GameScene: SKScene {
         self.physicsBody = border
     }
     
+    @objc func handleSwipe(_ gestureRecognizer: UIPanGestureRecognizer) {
+        let translation = gestureRecognizer.translation(in: view)
+        
+        switch gestureRecognizer.state {
+        case .changed:
+            // Move the node based on the swipe gesture
+            main.position.x += translation.x
+            gestureRecognizer.setTranslation(CGPoint.zero, in: view)
+            
+        default:
+            break
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            // Check if the touch is on the targetNode
+            if ball.contains(location) {
+                applyForceToPlayer()
+            }
+        }
+    }
+    
+    func applyForceToPlayer() {
+        // Apply force to playerNode
+        let newDx = -(ball.physicsBody?.velocity.dx)!
+        let force = CGVector(dx: -(ball.physicsBody?.velocity.dx)!, dy: -(ball.physicsBody?.velocity.dy)!)
+        ball.physicsBody?.applyForce(force)
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        let moveAction = SKAction.moveTo(x: ball.position.x, duration: 1.0)
+        enemy.run(moveAction)
     }
 }
